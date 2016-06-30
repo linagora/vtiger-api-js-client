f984ac03cf15f4e20f34176d61bb4770661c3b84
EddyTrotin <dev.eddy.trotin@outlook.com>
2016-06-30 17:29:31 +0200

    Added fluent API usage

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 let Client = require('./lib/Client');
-let Users = require('./lib/vTigerElements/Users');
-
-let localhost_clientOptions = {
-	url: 'http://localhost/vtigercrm/webservice.php?',
-	user : {
-		name: 'admin',
-		accessKey: 'sGBsf1zXF9tQ2bP7'
-	}
-};
+let Users = require('./lib/vTigerObjects/Users');
 
 let objectTest = new Users({
 	user_name: 'john_doe',
@@ -18,13 +10,16 @@ let objectTest = new Users({
     email1: 'john.doe@localhost',
 });
 
-let client = new Client(localhost_clientOptions);
+let client = new Client()
+.withAPIUrl('http://localhost/vtigercrm/webservice.php?')
+.withUserName('admin')
+.withUserAccessKey('sGBsf1zXF9tQ2bP7');
 client.connect()
 
 /********************Test CRUD operation on objectTest********************/
 
-.then((data) => {console.log(data);return client.create(objectTest)}, (err) => {return new Promise((resolve, reject) => {console.log(err);reject('');})})
-.then((data) => {console.log(data);return client.retrieve(new Users({id: data.result.id}))}, (err) => {return new Promise((resolve, reject) => { console.log(err);reject('');})})
-.then((data) => {console.log('Retrieve : ' + data.success);return client.update(new Users({user_name: 'chris_tucker', id: data.result.id}))}, (err) => {return new Promise((resolve, reject) => { console.log(err);reject('');})})
-.then((data) => {console.log('Update : ' + data.success);return client.delete(new Users({id: data.result.id}))}, (err) => {return new Promise((resolve, reject) => { console.log(err);reject('');})})
+.then((data) => {console.log(data); return client.create(objectTest)}, (err) => {return new Promise((resolve, reject) => {console.log(err);reject('');})})
+.then((data) => {console.log(data); return client.retrieve(new Users({id: data.result.id}))}, (err) => {return new Promise((resolve, reject) => { console.log(err);reject('');})})
+.then((data) => {console.log('Retrieve : ' + data.success); return client.update(new Users({user_name: 'chris_tucker', id: data.result.id}))}, (err) => {return new Promise((resolve, reject) => { console.log(err);reject('');})})
+.then((data) => {console.log('Update : ' + data.success); return client.delete(new Users({id: data.result.id}))}, (err) => {return new Promise((resolve, reject) => { console.log(err);reject('');})})
 .then((data) => {console.log('Delete : ' + data.success)}, (err) => {console.log(err)});
