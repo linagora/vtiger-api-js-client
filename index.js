da055ba7fc102d0d27898b3feaec05906de31697
EddyTrotin <dev.eddy.trotin@outlook.com>
2016-07-04 17:06:58 +0200

    Added index.js so that require('vtiger-client') works

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-let Client = require('./lib/Client');
-let Users = require('./lib/vTigerObjects/Users');
+'use strict';
 
-let objectTest = new Users({
-	user_name: 'john_doe',
-    user_password: 'adminadmin',  
-    confirm_password: 'adminadmin',  
-    last_name: 'Doe',  
-    roleid: 'H5',  
-    email1: 'john.doe@localhost',
-});
-
-let client = new Client()
-.withAPIUrl('http://localhost/vtigercrm/webservice.php?')
-.withUserName('admin')
-.withUserAccessKey('sGBsf1zXF9tQ2bP7');
-client.connect()
-
-/********************Test CRUD operation on objectTest********************/
-
-.then((data) => {console.log(data); return client.create(objectTest)}, (err) => {return new Promise((resolve, reject) => {console.log(err);reject('');})})
-.then((data) => {console.log(data); return client.retrieve(new Users({id: data.result.id}))}, (err) => {return new Promise((resolve, reject) => { console.log(err);reject('');})})
-.then((data) => {console.log('Retrieve : ' + data.success); return client.update(new Users({user_name: 'chris_tucker', id: data.result.id}))}, (err) => {return new Promise((resolve, reject) => { console.log(err);reject('');})})
-.then((data) => {console.log('Update : ' + data.success); return client.delete(new Users({id: data.result.id}))}, (err) => {return new Promise((resolve, reject) => { console.log(err);reject('');})})
-.then((data) => {console.log('Delete : ' + data.success)}, (err) => {console.log(err)});
+module.exports = exports = require('./dist/vtiger-client.min.js');
