aa7139b409532b0c10f896ecd44bc6a6bf51adbc
EddyTrotin <dev.eddy.trotin@outlook.com>
2016-07-29 15:13:34 +0200

    Code updated for ES5 compatibility using Q promise provider

--- samples/cli/vtiger-client.js
+++ samples/cli/vtiger-client.js
@@ -1,20 +1,19 @@
 'use strict';
 
-let Client = require('../../lib/Client');
-let Users = require('../../lib/models/Users');
-let vtiger_client = require('../../dist/vtiger_client.min');
+var vtiger_client = require('../../dist/vtiger-client');
 
-let objectTest = new Users({
+var objectTest = new vtiger_client.Users({
   user_name: 'john_doe',
   user_password: 'adminadmin',  
   confirm_password: 'adminadmin',  
   last_name: 'Doe',  
   roleid: 'H5',  
   email1: 'john.doe@localhost',
+  is_owner : false
 });
 
-let client = new Client(new vtiger_client.RequestTransport(), new vtiger_client.QPromiseProvider())
-.withUrl('http://localhost/vtigercrm/webservice.php?')
+var client = new vtiger_client.Client(new vtiger_client.RequestTransport(), new vtiger_client.QPromiseProvider())
+.withUrl('http://localhost/vtiger/webservice.php?')
 .withUserName('admin')
 .withUserAccessKey('sGBsf1zXF9tQ2bP7');
 
@@ -22,44 +21,40 @@ client.connect()
 
 /********************Test CRUD operations on objectTest********************/
 
-.then((data) => {
+/*.then(function(data) {
   console.log(data);
   return client.create(objectTest);
-}, (err) => {
-  return new Promise((resolve, reject) => {
-    console.log(err);
-    reject('');
-  });
-})
-.then((data) => {
-  console.log(data);
-  return client.retrieve(new Users({id: data.result.id}));
-}, (err) => {
-  return new Promise((resolve, reject) => {
-    console.log(err);
-    reject('');
-  });
-})
-.then((data) => {
-  console.log('Retrieve : ' + data.success);
-  return client.update(new Users({user_name: 'chris_tucker', id: data.result.id}));
-}, (err) => {
-  return new Promise((resolve, reject) => {
-    console.log(err);
-    reject('');
-  });
-})
-.then((data) => {
-  console.log('Update : ' + data.success);
-  return client.delete(new Users({id: data.result.id}));
-}, (err) => {
-  return new Promise((resolve, reject) => {
-    console.log(err);
-    reject('');
-  });
-})
-.then((data) => {
-  console.log('Delete : ' + data.success);
-}, (err) => {
+}, function(err) {
   console.log(err);
-});
+})
+.then(function(data) {
+  console.log(data);
+  return client.retrieve(new vtiger_client.Users({id: data.result.id}));
+}, function(err) {
+  console.log(err);
+})
+.then(function(data) {
+  console.log('Retrieve : ' + data.success);
+  return client.update(new vtiger_client.Users({user_name: 'chris_tucker', id: data.result.id}));
+}, function(err) {
+  console.log(err);
+})
+.then(function(data) {
+  console.log('Update : ' + data.success);
+  return client.devare(new vtiger_client.Users({id: data.result.id}));
+}, function(err) {
+  console.log(err);
+})*/
+.then(function(data) {
+  //console.log('Devare : ' + data.success);
+  console.log(data);
+  var sqlQuery = encodeURI("select * from Contacts where assigned_user_id='19x" + client.id + "';");
+  return client.query('query', '&query=' + sqlQuery);
+}, function(err) {
+  console.log(err);
+})
+.then(function(data) {
+  console.log(data);
+}, function(err) {
+  console.log(err);
+})
